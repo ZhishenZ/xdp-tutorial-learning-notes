bb88b312a1bbc75a20c3b0985384603365e596c0
ZhishenZ <clemenszhang@gmail.com>
2023-08-01 15:10:41 +0200

    packet 1

--- packet01-parsing/xdp_prog_kern.c
+++ packet01-parsing/xdp_prog_kern.c
@@ -4,6 +4,7 @@
 #include <linux/in.h>
 #include <linux/if_ether.h>
 #include <linux/if_packet.h>
+#include <linux/ip.h>
 #include <linux/ipv6.h>
 #include <linux/icmpv6.h>
 #include <bpf/bpf_helpers.h>
@@ -17,6 +18,42 @@ struct hdr_cursor {
 	void *pos;
 };
 
+/*
+ *	struct vlan_hdr - vlan header
+ *	@h_vlan_TCI: priority and VLAN ID
+ *	@h_vlan_encapsulated_proto: packet type ID or len
+ */
+struct vlan_hdr {
+	__be16	h_vlan_TCI;
+	__be16	h_vlan_encapsulated_proto;
+};
+
+/*
+ * Struct icmphdr_common represents the common part of the icmphdr and icmp6hdr
+ * structures.
+ */
+struct icmphdr_common {
+	__u8		type;
+	__u8		code;
+	__sum16	cksum;
+};
+/* Allow users of header file to redefine VLAN max depth */
+#ifndef VLAN_MAX_DEPTH
+#define VLAN_MAX_DEPTH 2
+#endif
+
+#define VLAN_VID_MASK		0x0fff /* VLAN Identifier */
+/* Struct for collecting VLANs after parsing via parse_ethhdr_vlan */
+struct collect_vlans {
+	__u16 id[VLAN_MAX_DEPTH];
+};
+
+static __always_inline int proto_is_vlan(__u16 h_proto)
+{
+	return !!(h_proto == bpf_htons(ETH_P_8021Q) ||
+		  h_proto == bpf_htons(ETH_P_8021AD));
+}
+
 /* Packet parsing helpers.
  *
  * Each helper parses a packet header, including doing bounds checking, and
@@ -26,45 +63,137 @@ struct hdr_cursor {
  * (h_proto for Ethernet, nexthdr for IPv6), for ICMP it is the ICMP type field.
  * All return values are in host byte order.
  */
-static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
-					void *data_end,
-					struct ethhdr **ethhdr)
+static __always_inline int parse_ethhdr_vlan(struct hdr_cursor *nh,
+					     void *data_end,
+					     struct ethhdr **ethhdr,
+					     struct collect_vlans *vlans)
 {
 	struct ethhdr *eth = nh->pos;
 	int hdrsize = sizeof(*eth);
+	struct vlan_hdr *vlh;
+	__u16 h_proto;
+	int i;
 
 	/* Byte-count bounds check; check if current pointer + size of header
 	 * is after data_end.
 	 */
-	if (nh->pos + 1 > data_end)
+	if (nh->pos + hdrsize > data_end)
 		return -1;
 
 	nh->pos += hdrsize;
 	*ethhdr = eth;
+	vlh = nh->pos;
+	h_proto = eth->h_proto;
 
-	return eth->h_proto; /* network-byte-order */
+	/* Assignment 4, add VLAN support */
+	/* Use loop unrolling to avoid the verifier restriction on loops;
+	 * support up to VLAN_MAX_DEPTH layers of VLAN encapsulation.
+	 */
+	#pragma unroll
+	for ( i = 0; i < VLAN_MAX_DEPTH; i++) {
+		if (!proto_is_vlan(h_proto))
+			break;
+
+		if (vlh + 1 > data_end)
+			break;
+
+		h_proto = vlh->h_vlan_encapsulated_proto;
+		if (vlans) /* collect VLAN ids */
+			vlans->id[i] =
+				(bpf_ntohs(vlh->h_vlan_TCI) & VLAN_VID_MASK);
+
+		vlh++;
+	}
+
+	nh->pos = vlh;
+	return h_proto; /* network-byte-order */
+}
+
+static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
+					void *data_end,
+					struct ethhdr **ethhdr)
+{
+	/* Expect compiler removes the code that collects VLAN ids */
+	return parse_ethhdr_vlan(nh, data_end, ethhdr, NULL);
 }
 
 /* Assignment 2: Implement and use this */
-/*static __always_inline int parse_ip6hdr(struct hdr_cursor *nh,
+static __always_inline int parse_ip6hdr(struct hdr_cursor *nh,
 					void *data_end,
 					struct ipv6hdr **ip6hdr)
 {
-}*/
+	struct ipv6hdr *ip6h = nh->pos;
+
+	/* Pointer-arithmetic bounds check; pointer +1 points to after end of
+	 * thing being pointed to. We will be using this style in the remainder
+	 * of the tutorial.
+	 */
+	if (ip6h + 1 > data_end)
+		return -1;
+
+	nh->pos = ip6h + 1;
+	*ip6hdr = ip6h;
+
+	return ip6h->nexthdr;
+}
 
 /* Assignment 3: Implement and use this */
-/*static __always_inline int parse_icmp6hdr(struct hdr_cursor *nh,
+static __always_inline int parse_icmp6hdr(struct hdr_cursor *nh,
 					  void *data_end,
 					  struct icmp6hdr **icmp6hdr)
 {
-}*/
+	struct icmp6hdr *icmp6h = nh->pos;
+
+	if (icmp6h + 1 > data_end)
+		return -1;
+
+	nh->pos = icmp6h + 1;
+	*icmp6hdr = icmp6h;
+
+	return icmp6h->icmp6_type;
+}
+
+/* Assignment 5: Add IPv4 support */
+static __always_inline int parse_iphdr(struct hdr_cursor *nh,
+										void *data_end,
+										struct iphdr **iphdr)
+{
+
+	struct iphdr *iph = nh->pos;
+	int hdrsize;
+
+	if ( iph + 1 > data_end)
+		return -1;
+
+	/* nihl:4 stands for the Internet Header Length (IHL) field.
+	 * It is a 4-bit field that represents the length of the IPv4 header in 32-bit words.
+	   multiplies 4 to change the word to bytes */
+	hdrsize = iph->ihl * 4;
+
+	/* Sanity check packet filed is valid */
+	if(hdrsize < sizeof(*iph))
+		return -1;
+
+	/* Variable-length IPv4 header, need to use byte-based arithmetic */
+	if (nh->pos + hdrsize > data_end)
+		return -1;
+
+	nh->pos += hdrsize;
+	*iphdr = iph;
+
+	return iph->protocol;
+
+}
+
+
 
 SEC("xdp")
 int  xdp_parser_func(struct xdp_md *ctx)
 {
 	void *data_end = (void *)(long)ctx->data_end;
 	void *data = (void *)(long)ctx->data;
-	struct ethhdr *eth;
+	struct ethhdr *ethhdr;
+	struct iphdr *iphdr;
 
 	/* Default action XDP_PASS, imply everything we couldn't parse, or that
 	 * we don't want to deal with, we just pass up the stack and let the
@@ -83,13 +212,46 @@ int  xdp_parser_func(struct xdp_md *ctx)
 	 * parsing fails. Each helper function does sanity checking (is the
 	 * header type in the packet correct?), and bounds checking.
 	 */
-	nh_type = parse_ethhdr(&nh, data_end, &eth);
-	if (nh_type != bpf_htons(ETH_P_IPV6))
+	nh_type = parse_ethhdr(&nh, data_end, &ethhdr);
+
+	if (nh_type == bpf_htons(ETH_P_IP))
+	{
+		/* ipv4 if it is a */
+		nh_type = parse_iphdr(&nh, data_end, &iphdr);
+		// why pass a pointer of a pointer into a function?
+		// pass a pointer of a pointer into a function, it allows the function to change the function itself.
+
+		/* For test usage, if the network package is an ICMP packet which is
+		 * typically used for ping commands, we drop the network packet. */
+		if (nh_type != IPPROTO_ICMP)
+			goto out;
+		else
+			action = XDP_DROP;
+	}
+	else if (nh_type == bpf_htons(ETH_P_IPV6))
+	{
+		struct ipv6hdr *ip6h;
+		struct icmp6hdr *icmp6h;
+
+		nh_type = parse_ip6hdr(&nh, data_end, &ip6h);
+		// in this case, we bypass the IPV6 packages if (nh_type == IPPROTO_ICMPV6)
+		if (nh_type != IPPROTO_ICMPV6)
+			goto out;
+
+		// we let all the "Echo Request" messages of a ICMPv6 packet bypass
+		nh_type = parse_icmp6hdr(&nh, data_end, &icmp6h);
+		if (nh_type != ICMPV6_ECHO_REQUEST)
+			goto out;
+
+		// return XDP_DROP if the sequence number is even, and XDP_PASS otherwise.
+		if (bpf_ntohs(icmp6h->icmp6_sequence) % 2 == 0)
+			action = XDP_DROP;
+	}
+	else
+	{
 		goto out;
+	}
 
-	/* Assignment additions go below here */
-
-	action = XDP_DROP;
 out:
 	return xdp_stats_record_action(ctx, action); /* read via xdp_stats */
 }
